239c314156e7b552a14511a77cf2c6b11fed060d
domingosmpbrasil <92321458+domingosmpbrasil@users.noreply.github.com>
2024-06-22 19:30:04 -0300

    Criado método sacar e depositar

--- JavaPOO/BancoJava/src/Conta.java
+++ JavaPOO/BancoJava/src/Conta.java
@@ -1,6 +1,26 @@
 public class Conta {
-    double saldo = 1000.00;
-    int agencia = 0001;
-    int numeroConta = 25253;
-    String nomeTitular = "Miguel";
+    double saldo;
+    int agencia;
+    int numeroConta;
+    String nomeTitular;
+
+    public void depositar(double valor) {
+        saldo += valor;
+    }
+
+    public boolean sacar(double valor) {
+        if (saldo >= valor) {
+            saldo -= valor;
+            return true;
+        }
+        return false;
+    }
+
+    public void extrato(){
+        System.out.println("Saldo: "+ saldo);
+        System.out.println("Agencia: " + agencia);
+        System.out.println("Número da conta: " + numeroConta);
+        System.out.println("Titular:" + nomeTitular);
+    }
+
 }
--- JavaPOO/BancoJava/src/Main.java
+++ JavaPOO/BancoJava/src/Main.java
@@ -1,6 +1,40 @@
+
+
 public class Main {
     public static void main(String[] args) {
+        Conta conta1 = new Conta();
 
-        System.out.println("Hello world!");
+        conta1.saldo = 1000.0;
+        conta1.agencia = 1001;
+        conta1.numeroConta = 12345;
+        conta1.nomeTitular = "João";
+        System.out.println("Saldo: "+ conta1.saldo);
+        System.out.println("Agencia: " + conta1.agencia);
+        System.out.println("Número da conta: " + conta1.numeroConta);
+        System.out.println("Titular:" + conta1.nomeTitular);
+
+
+        Conta conta2 = new Conta();
+
+        conta2.saldo = 10000.0;
+        conta2.agencia = 1001;
+        conta2.numeroConta = 12346;
+        conta2.nomeTitular = "Maria";
+        System.out.println("Saldo: "+ conta2.saldo);
+        System.out.println("Agencia: " + conta2.agencia);
+        System.out.println("Número da conta: " + conta2.numeroConta);
+        System.out.println("Titular:" + conta2.nomeTitular);
+
+        //conta 1 - João
+        System.out.println("\n");
+        conta1.depositar(100.00);
+        conta1.extrato();
+
+        // Conta 2 - Maria
+        System.out.println("\n");
+        conta2.sacar(4000.00);
+        conta2.extrato();
+
+
     }
 }
